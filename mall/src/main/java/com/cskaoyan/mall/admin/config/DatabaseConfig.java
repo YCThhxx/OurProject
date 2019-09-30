39a1726f7f3fdb3655e61fdca1f8bc1314c097a4
YCThhxx <1192283783@qq.com>
2019-09-30 20:18:59 +0800

    yct：增加事务配置

---
+++ mall/src/main/java/com/cskaoyan/mall/admin/config/DatabaseConfig.java
@@ -0,0 +1,18 @@
+package com.cskaoyan.mall.admin.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Bean
+    public DataSourceTransactionManager dataSourceTransactionManager(DataSource dataSource){
+        DataSourceTransactionManager dataSourceTransactionManager = new DataSourceTransactionManager();
+        dataSourceTransactionManager.setDataSource(dataSource);
+        return dataSourceTransactionManager;
+    }
+}
--- mall/src/test/java/com/cskaoyan/mall/MallApplicationTests.java
+++ mall/src/test/java/com/cskaoyan/mall/MallApplicationTests.java
@@ -13,4 +13,5 @@ public class MallApplicationTests {
     public void contextLoads() {
     }
 
+
 }

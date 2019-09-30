0cfb235e619153924c83cde56194a46b4cdd1676
chenwenlong <13427537790@163.com>
2019-09-30 16:00:07 +0800

    cwl

--- mall/src/main/java/com/cskaoyan/mall/config/MyDatasourceConfig.java
+++ mall/src/main/java/com/cskaoyan/mall/config/MyDatasourceConfig.java
@@ -4,10 +4,12 @@ import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 
 @Configuration
+@Component
 @ConfigurationProperties(prefix = "my.ds")
 public class MyDatasourceConfig {
 
---
+++ mall/src/main/java/com/cskaoyan/mall/vo/ConfigVo.java
@@ -0,0 +1,170 @@
+package com.cskaoyan.mall.vo;
+
+public class ConfigVo {
+    private String litemall_mall_address;
+    private String litemall_mall_name;
+    private String litemall_mall_phone;
+    private String litemall_mall_qq;
+    private int litemall_express_freight_min;
+    private int litemall_express_freight_value;
+    private int litemall_order_comment;
+    private int litemall_order_unconfirm;
+    private int litemall_order_unpaid;
+    private int litemall_wx_catlog_goods;
+    private int litemall_wx_catlog_list;
+    private int litemall_wx_index_brand;
+    private int litemall_wx_index_hot;
+    private int litemall_wx_index_new;
+    private int litemall_wx_index_topic;
+    private boolean litemall_wx_share;
+
+    public String getLitemall_mall_address() {
+        return litemall_mall_address;
+    }
+
+    public void setLitemall_mall_address(String litemall_mall_address) {
+        this.litemall_mall_address = litemall_mall_address;
+    }
+
+    public String getLitemall_mall_name() {
+        return litemall_mall_name;
+    }
+
+    public void setLitemall_mall_name(String litemall_mall_name) {
+        this.litemall_mall_name = litemall_mall_name;
+    }
+
+    public String getLitemall_mall_phone() {
+        return litemall_mall_phone;
+    }
+
+    public void setLitemall_mall_phone(String litemall_mall_phone) {
+        this.litemall_mall_phone = litemall_mall_phone;
+    }
+
+    public String getLitemall_mall_qq() {
+        return litemall_mall_qq;
+    }
+
+    public void setLitemall_mall_qq(String litemall_mall_qq) {
+        this.litemall_mall_qq = litemall_mall_qq;
+    }
+
+    public int getLitemall_express_freight_min() {
+        return litemall_express_freight_min;
+    }
+
+    public void setLitemall_express_freight_min(int litemall_express_freight_min) {
+        this.litemall_express_freight_min = litemall_express_freight_min;
+    }
+
+    public int getLitemall_express_freight_value() {
+        return litemall_express_freight_value;
+    }
+
+    public void setLitemall_express_freight_value(int litemall_express_freight_value) {
+        this.litemall_express_freight_value = litemall_express_freight_value;
+    }
+
+    public int getLitemall_order_comment() {
+        return litemall_order_comment;
+    }
+
+    public void setLitemall_order_comment(int litemall_order_comment) {
+        this.litemall_order_comment = litemall_order_comment;
+    }
+
+    public int getLitemall_order_unconfirm() {
+        return litemall_order_unconfirm;
+    }
+
+    public void setLitemall_order_unconfirm(int litemall_order_unconfirm) {
+        this.litemall_order_unconfirm = litemall_order_unconfirm;
+    }
+
+    public int getLitemall_order_unpaid() {
+        return litemall_order_unpaid;
+    }
+
+    public void setLitemall_order_unpaid(int litemall_order_unpaid) {
+        this.litemall_order_unpaid = litemall_order_unpaid;
+    }
+
+    public int getLitemall_wx_catlog_goods() {
+        return litemall_wx_catlog_goods;
+    }
+
+    public void setLitemall_wx_catlog_goods(int litemall_wx_catlog_goods) {
+        this.litemall_wx_catlog_goods = litemall_wx_catlog_goods;
+    }
+
+    public int getLitemall_wx_catlog_list() {
+        return litemall_wx_catlog_list;
+    }
+
+    public void setLitemall_wx_catlog_list(int litemall_wx_catlog_list) {
+        this.litemall_wx_catlog_list = litemall_wx_catlog_list;
+    }
+
+    public int getLitemall_wx_index_brand() {
+        return litemall_wx_index_brand;
+    }
+
+    public void setLitemall_wx_index_brand(int litemall_wx_index_brand) {
+        this.litemall_wx_index_brand = litemall_wx_index_brand;
+    }
+
+    public int getLitemall_wx_index_hot() {
+        return litemall_wx_index_hot;
+    }
+
+    public void setLitemall_wx_index_hot(int litemall_wx_index_hot) {
+        this.litemall_wx_index_hot = litemall_wx_index_hot;
+    }
+
+    public int getLitemall_wx_index_new() {
+        return litemall_wx_index_new;
+    }
+
+    public void setLitemall_wx_index_new(int litemall_wx_index_new) {
+        this.litemall_wx_index_new = litemall_wx_index_new;
+    }
+
+    public int getLitemall_wx_index_topic() {
+        return litemall_wx_index_topic;
+    }
+
+    public void setLitemall_wx_index_topic(int litemall_wx_index_topic) {
+        this.litemall_wx_index_topic = litemall_wx_index_topic;
+    }
+
+    public boolean isLitemall_wx_share() {
+        return litemall_wx_share;
+    }
+
+    public void setLitemall_wx_share(boolean litemall_wx_share) {
+        this.litemall_wx_share = litemall_wx_share;
+    }
+
+    @Override
+    public String toString() {
+        return "ConfigVo{" +
+                "litemall_mall_address='" + litemall_mall_address + '\'' +
+                ", litemall_mall_name='" + litemall_mall_name + '\'' +
+                ", litemall_mall_phone='" + litemall_mall_phone + '\'' +
+                ", litemall_mall_qq='" + litemall_mall_qq + '\'' +
+                ", litemall_express_freight_min=" + litemall_express_freight_min +
+                ", litemall_express_freight_value=" + litemall_express_freight_value +
+                ", litemall_order_comment=" + litemall_order_comment +
+                ", litemall_order_unconfirm=" + litemall_order_unconfirm +
+                ", litemall_order_unpaid=" + litemall_order_unpaid +
+                ", litemall_wx_catlog_goods=" + litemall_wx_catlog_goods +
+                ", litemall_wx_catlog_list=" + litemall_wx_catlog_list +
+                ", litemall_wx_index_brand=" + litemall_wx_index_brand +
+                ", litemall_wx_index_hot=" + litemall_wx_index_hot +
+                ", litemall_wx_index_new=" + litemall_wx_index_new +
+                ", litemall_wx_index_topic=" + litemall_wx_index_topic +
+                ", litemall_wx_share=" + litemall_wx_share +
+                '}';
+    }
+}

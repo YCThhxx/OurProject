070f05aadadef9ea2445ceebd73530ac063b6413
linguoqing <466795460@qq.com>
2019-09-30 23:23:39 +0800

    对象存储接口

--- mall/src/main/java/com/cskaoyan/mall/admin/mapper/CskaoyanMallStorageMapper.java
+++ mall/src/main/java/com/cskaoyan/mall/admin/mapper/CskaoyanMallStorageMapper.java
@@ -1,8 +1,14 @@
 package com.cskaoyan.mall.admin.mapper;
 
 import com.cskaoyan.mall.admin.bean.CskaoyanMallStorage;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface CskaoyanMallStorageMapper {
+
+    List<CskaoyanMallStorage> queryStorage();
+
     int deleteByPrimaryKey(Integer id);
 
     CskaoyanMallStorage selectByPrimaryKey(Integer id);
@@ -10,4 +16,6 @@ public interface CskaoyanMallStorageMapper {
     int updateByPrimaryKeySelective(CskaoyanMallStorage record);
 
     int updateByPrimaryKey(CskaoyanMallStorage record);
+
+    int countAll();
 }
